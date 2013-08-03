aa74700d03d2aac802d742da7a1b8c2176c6d3f3
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-08-03 15:06:55 +0200

    Update app assembly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var di = require('di');
 
+var ApiBuilder = require('./source/api-builder');
 var FileService = require('./source/file-service.js');
 var ServiceFactory = require('./source/service-factory.js');
 var ServiceLoader = require('./source/service-loader.js');
@@ -9,6 +10,7 @@ var fs = require('fs');
 var process = require('child_process');
 
 var module = {
+	'apiBuilder': ['type', ApiBuilder],
 	'fileService' : ['type', FileService],
 	'serviceLoader' : ['type', ServiceLoader],
 	'service': ['type', Service],
@@ -20,7 +22,9 @@ var module = {
 
 var injector = new di.Injector([module]);
 
-injector.invoke(function (serviceLoader) {
-	var services = serviceLoader.load('services');
+injector.invoke(function (apiBuilder) {
+
+	var api = apiBuilder.build('services');
+	console.log(api);
 
 });
